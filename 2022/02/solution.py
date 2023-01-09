cd64f1fa158e050775bc75ad97d451ba4660ea38
Felipe Tadeu <souzalima@gmail.com>
2023-01-09 20:24:39 -0300

    first solution 2022/02

--- 2022/02/solution.py
+++ 2022/02/solution.py
@@ -1,14 +1,40 @@
+def points_per_round(round_string):
+    opponent_shape = map_input[round_string[0]]
+    our_shape = map_input[round_string[2]]
+
+    if opponent_shape == our_shape:
+        return points_per_outcome["Draw"] + points_per_shape[our_shape]
+    elif (opponent_shape, our_shape) in [
+        ("Paper", "Rock"),
+        ("Rock", "Scissors"),
+        ("Scissors", "Paper"),
+    ]:
+        # loss for us
+        return points_per_outcome["Lose"] + points_per_shape[our_shape]
+    else:
+        return points_per_outcome["Win"] + points_per_shape[our_shape]
+
 
 def solve():
 
-    file = open('input', 'r')
-    Lines = file.readlines()
-    
-    for line in Lines:
-        else:
+    file = open("input", "r")
+    lines = file.readlines()
+    rounds = [entry.strip() for entry in lines]
 
-        
+    print(sum([points_per_round(round_string) for round_string in rounds]))
 
 
 if __name__ == "__main__":
+
+    map_input = {
+        "A": "Rock",
+        "B": "Paper",
+        "C": "Scissors",
+        "X": "Rock",
+        "Y": "Paper",
+        "Z": "Scissors",
+    }
+    points_per_shape = {"Rock": 1, "Paper": 2, "Scissors": 3}
+    points_per_outcome = {"Lose": 0, "Draw": 3, "Win": 6}
+
     solve()
